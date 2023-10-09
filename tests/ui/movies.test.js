0d7fb8291261b4c78f4b93587273e1faf4792d2b
newlolacc <newlolacc1465@gmail.com>
2023-10-09 21:57:32 +0300

    Change url in all ui tests

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Check movies page", async ({ page }) => {
-  await page.goto("http://localhost:8080/movies");
+  await page.goto("https://ivangeorgiev34-movies.onrender.com");
   const list = await page.$("ul");
   expect(list).toBeTruthy();
 });
