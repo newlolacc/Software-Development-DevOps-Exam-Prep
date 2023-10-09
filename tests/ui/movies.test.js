1217085386afea4089ab881eee6bf386f72fa688
newlolacc <newlolacc1465@gmail.com>
2023-10-09 22:09:53 +0300

    Change movies tests

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Check movies page", async ({ page }) => {
-  await page.goto("https://ivangeorgiev34-movies.onrender.com");
+  await page.goto("https://ivangeorgiev34-movies.onrender.com/movies");
   const list = await page.$("ul");
   expect(list).toBeTruthy();
 });
